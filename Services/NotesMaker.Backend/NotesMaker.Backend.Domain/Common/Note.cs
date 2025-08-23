6a83c6b2ee05f438f91e376b7506f84be42054fb
Evgeny Kuprienko <evgeny.kuprienko000@gmail.com>
2025-08-23 18:31:19 +0300

    Add domainl ayer

---
+++ Services/NotesMaker.Backend/NotesMaker.Backend.Domain/Common/Note.cs
@@ -0,0 +1,15 @@
+﻿namespace NotesMaker.Backend.Domain.Common
+{
+    public class Note
+    {
+        public Guid Uid { get; set; }
+        public Guid Owner { get; set; }
+        public string? Tittle { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Updated { get; set; }
+        public string? Content { get; set; }
+        public bool IsDeleted { get; set; }
+        public IEnumerable<NoteAccess> NoteAccess { get; set; } 
+            = Enumerable.Empty<NoteAccess>();
+    }
+}
---
+++ Services/NotesMaker.Backend/NotesMaker.Backend.Domain/Common/NoteAccess.cs
@@ -0,0 +1,13 @@
+﻿
+namespace NotesMaker.Backend.Domain.Common
+{
+    public class NoteAccess
+    {
+        public Guid Uid { get; set; }
+        public Guid UserUid { get; set; }
+        public Guid NoteUid { get; set; }
+        public bool Read { get; set; }
+        public bool Write { get; set; }
+        private bool Delete { get; set; }
+    }
+}
